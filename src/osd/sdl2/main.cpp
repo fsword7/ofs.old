b75ba47c29b163f26c39d0eb6a7dc5a1b71eb7bb
Timothy Stark <fsword007@gmail.com>
2018-10-02 11:09:18 -0400

    Added SDL2 main program and initial CMakeLists.txt

---
+++ src/osd/sdl2/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp - SDL2 main program
+ *
+ *  Created on: Oct 2, 2018
+ *      Author: Tim Stark
+ */
+
+#include <iostream>
+
+#include "main/main.h"
+
+int main(int argc, char **argv)
+{
+	std::cout << "Orbital Flight Simulator" << std::endl;
+
+	return 0;
+}
