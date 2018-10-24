83cfe9212941c3c5f206e11536acb96744a60619
Timothy Stark <fsword007@gmail.com>
2018-10-23 21:36:20 -0400

    Added CelestialStar class facility as initial implementation.

---
+++ src/engine/universe/star.cpp
@@ -0,0 +1,21 @@
+/*
+ * star.cpp
+ *
+ *  Created on: Oct 23, 2018
+ *      Author: Tim Stark
+ */
+
+#include "main/main.h"
+#include "main/math.h"
+#include "engine/universe/star.h"
+
+CelestialStar::CelestialStar(string& name)
+: Object(name, ObjectType::objCelestialStar)
+{
+
+}
+
+CelestialStar::~CelestialStar()
+{
+}
+
---
+++ src/engine/universe/star.h
@@ -0,0 +1,20 @@
+/*
+ * star.h
+ *
+ *  Created on: Oct 23, 2018
+ *      Author: Tim Stark
+ */
+
+#pragma once
+
+#include "engine/object.h"
+
+class CelestialStar : public Object
+{
+public:
+	 CelestialStar(string& name);
+	 ~CelestialStar();
+
+private:
+
+};
