86ab0b742ca01a78c2c091ceb33d7d68c0027ede
Timothy Stark <fsword007@gmail.com>
2019-04-04 12:54:13 -0400

    Added some routines.

--- src/universe/star.cpp
+++ src/universe/star.cpp
@@ -6,10 +6,33 @@
  */
 
 #include "main/core.h"
+#include "universe/astro.h"
 #include "universe/star.h"
 
+//#include "main/math.h"
+//#include "main/eigen.h"
+//#include "engine/object.h"
+//#include "engine/universe/astro.h"
+//#include "engine/universe/body.h"
+//#include "engine/universe/star.h"
+//#include "engine/universe/solarsys.h"
+
+CelestialStar::CelestialStar()
+: Object(ObjectType::objCelestialStar),
+  flags(0), lpos(0, 0, 0), ra(0.0), dec(0.0), plx(0.0),
+  absMag(0.0), bMag(0.0), vMag(0.0),
+  ci(0.0), lum(0.0), temp(0)
+//  system(nullptr)
+{
+
+}
+
 CelestialStar::CelestialStar(string& name)
-: Object(name, ObjectType::objCelestialStar)
+: Object(name, ObjectType::objCelestialStar),
+  flags(0), lpos(0, 0, 0), ra(0.0), dec(0.0), plx(0.0),
+  absMag(0.0), bMag(0.0), vMag(0.0),
+  ci(0.0), lum(0.0), temp(0)
+//  system(nullptr)
 {
 
 }
@@ -18,3 +41,89 @@ CelestialStar::~CelestialStar()
 {
 }
 
+
+double CelestialStar::getLuminosity() const
+{
+	return astro::convertAbsMagToLum(absMag);
+}
+
+int CelestialStar::getTemperature() const
+{
+	double ci = 0.92 * this->ci;
+
+	return (int)(4600 * ((1 / (ci + 1.7)) + (1 / (ci + 0.62))));
+}
+
+double CelestialStar::getRadius() const
+{
+	if (flags & knownRadius)
+		return radius;
+
+	// Determine estimate star radius by using that formula
+	// R/Rs = (Ts/T)^2 (L/Ls)^1/2
+	return square(SOLAR_TEMPERATURE / getTemperature()) *
+		sqrt(getLuminosity() / SOLAR_LUMINOSITY) *
+		SOLAR_RADIUS;
+}
+
+vec3d_t CelestialStar::getPosition(double) const
+{
+	return lpos;
+}
+
+double CelestialStar::getAppMag(double dist) const
+{
+	return astro::convertAbsToAppMag(absMag, dist);
+}
+
+//void CelestialStar::setSolarSystem(SolarSystem *sys)
+//{
+//	system = sys;
+//}
+
+CelestialStar *CelestialStar::create(double ra, double dec, double plx,
+		const char *spType, double appMag, double ci, double lum)
+{
+	CelestialStar *star;
+	double pc;
+	int    temp;
+
+	star = new CelestialStar();
+	pc = 1000.0 / plx;
+	star->lpos = astro::convertEquatorialToEcliptic(ra, dec, pc);
+
+	temp = (int)(4600 * (1/((ci*0.92)+1.7) + 1/((ci*0.92)+0.62)));
+
+	star->ra  = ra;
+	star->dec = dec;
+	star->plx = plx;
+    star->absMag = astro::convertAppToAbsMag(appMag, pc);
+    star->bMag = appMag + ci;
+    star->vMag = appMag;
+	star->ci  = ci;
+	star->lum = lum;
+	star->temp = temp;
+
+	return star;
+}
+
+CelestialStar *CelestialStar::createSun()
+{
+	CelestialStar *star = new CelestialStar();
+
+	star->setName("Sol");
+	star->ra       = 0.0;
+	star->dec      = 0.0;
+	star->plx      = 0.0;
+	star->absMag   = SOLAR_ABSMAG;
+	star->bMag     = 0.0;
+	star->vMag     = 0.0;
+	star->ci       = SOLAR_CI;
+	star->lum      = SOLAR_LUMINOSITY;
+	star->temp     = SOLAR_TEMPERATURE;
+
+	star->flags    = knownRadius;
+	star->radius   = SOLAR_RADIUS;
+
+	return star;
+}
--- src/universe/star.h
+++ src/universe/star.h
@@ -9,12 +9,91 @@
 
 #include "engine/object.h"
 
+//// default solar parameters (our sun)
+////#define SOLAR_ABSMAG		4.83
+////#define SOLAR_IRRADIANCE	1367.6
+////#define SOLAR_POWER			3.8462e26
+////#define SOLAR_LUMINOSITY	1.0
+////#define SOLAR_TEMPERATURE	5777.0
+////#define SOLAR_CI			0.656
+////#define SOLAR_RADIUS		696000.0
+////#define SOLAR_MASS			1.9885e30
+
+enum SpectralClass
+{
+    Spectral_Unknown = 0,
+    Spectral_O  = 1,
+    Spectral_B  = 2,
+    Spectral_A  = 3,
+    Spectral_F  = 4,
+    Spectral_G  = 5,
+    Spectral_K  = 6,
+    Spectral_M  = 7,
+    Spectral_R  = 8,
+    Spectral_S  = 9,
+    Spectral_N  = 10,
+    Spectral_WC = 11,
+    Spectral_WN = 12,
+    Spectral_L  = 13,
+    Spectral_T  = 14,
+    Spectral_C  = 15,
+    Spectral_DA = 16,
+    Spectral_DB = 17,
+    Spectral_DC = 18,
+    Spectral_DO = 19,
+    Spectral_DQ = 20,
+    Spectral_DZ = 21,
+    Spectral_D  = 22,
+    Spectral_DX = 23
+};
+
 class CelestialStar : public Object
 {
 public:
-	 CelestialStar(string& name);
-	 ~CelestialStar();
+	enum {
+		knownRadius   = 1,
+		knownRotation = 2,
+		knownSurface  = 4
+	};
+
+	CelestialStar();
+	CelestialStar(string& name);
+	~CelestialStar();
+
+	int    getTemperature() const;
+	double getLuminosity() const;
+	double getRadius() const;
+	double getAppMag(double dist) const;
+
+	inline double getAbsMag() const { return absMag; }
+//	inline SolarSystem *getSolarSystem() const { return system; }
+
+	vec3d_t getPosition(double tjd) const;
+
+
+//	void setSolarSystem(SolarSystem *sys);
+
+	static CelestialStar *create(double ra, double dec, double plx, const char *spType,
+								 double appMag, double ci, double lum);
+	static CelestialStar *createSun();
 
 private:
+	// Star known flags
+	uint32_t flags;
+
+	// Star position/velocity
+	vec3d_t  lpos;    // Local position
+	double   ra, dec, plx;
+
+	// Spectral class/type
+	std::string  specName;
+	std::string  specType;
+
+	// Star atmosphere parameters
+	double radius;
+	double absMag;
+	double bMag, vMag;
+	double ci, lum;
+	int    temp;
 
 };
