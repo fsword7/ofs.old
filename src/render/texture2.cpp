387ca7a127c6ab4ce90d48f61feb9ebf9b7a5d9a
Timothy Stark <fsword007@gmail.com>
2019-07-28 19:28:13 -0400

    New Texture class package (merged old Image and Texture)

---
+++ src/render/texture2.cpp
@@ -0,0 +1,19 @@
+/*
+ * texture2.cpp
+ *
+ *  Created on: Jul 28, 2019
+ *      Author: Tim Stark
+ */
+
+#include "main/core.h"
+#include "render/texture2.h"
+
+uint8_t *Texture2::getMipData(int lod)
+{
+	return nullptr;
+}
+
+int Texture2::getMipSize(int lod)
+{
+	return 0;
+}
